debd690ac9c5bba7e4729bb9abad9fcf8422a138
lferreram <lferreram@yahoo.com>
2023-05-15 09:54:20 -0600

    first commit

---
+++ funciones1.js
@@ -0,0 +1,12 @@
+let num=0
+//cree una funcion que reciba un monto y le calcule su porcentaje
+//de IVA,devolviendo el monto del IVA correspondiente
+
+
+
+function montoIVA(numero,porcentaje)
+{return numero*porcentaje/100}
+console.log("MontoIVA", montoIVA(1000,20));
+
+
+
